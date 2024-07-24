21d903c00b9cc069d0393ae0e89b0ceb95b4a741
Akshat Bajetha <akshatbajetha@gmail.com>
2024-07-25 00:10:42 +0530

    update app.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -38,7 +38,7 @@ function App() {
       <div className="bg-[#172842] min-h-screen py-8">
         <div className="w-full max-w-2xl mx-auto shadow-md rounded-lg px-4 py-3 text-white">
           <h1 className="text-2xl font-bold text-center mb-8 mt-2">
-            To-do List
+            Taskly
           </h1>
           <div className="mb-4">
             <TodoForm />
